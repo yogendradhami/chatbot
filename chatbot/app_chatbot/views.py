9111fd7ea2c362f2d0a41ed4d614252d609cfdf8
yogendradhami <yogendradhami6@gmail.com>
2023-05-05 15:26:29 +0545

    completed

--- chatbot/app_chatbot/views.py
+++ chatbot/app_chatbot/views.py
@@ -1,8 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from chatterbot import  ChatBot
-from chatterbot.trainers import ListTrainer
+from chatterbot.trainers import ListTrainer,chatterBotCorpusTrainer
 # Create your views here.
+
+bot = ChatBot('chatbot', read_only= False,
+              logic_adapters=[
+                  {
+                  'import_path':'chatterbot.logic.BestMatch'
+                #   'default_response':"Sorry, I don't know what that means.",
+                #   'maximum_similarity_threshold':0.95,
+                  }])
+
+list_to_train=[
+    "hi", # question
+    "hi, there", # answer
+    "what's your name?", #
+    "I'm just a chatbot",
+    "What is your favourite food ?",
+    "I like cheese",
+    "What's your favourite sport?",
+    "swimming",
+    "do you have children?",
+    "no"
+]
+
+chatterbotCorpusTrainer = chatterBotCorpusTrainer(bot)
+
+# list_tainer = ListTrainer(bot)
+# list_tainer.train(list_to_train)
+chatterbotCorpusTrainer.train('chatterbot.corpus.english')
+
 
 def Index(request):
     return HttpResponse("this is my first url.")
@@ -12,4 +40,5 @@ def Specific(request):
 
 def getResponse(request):
     userMessage=request.GET.get('userMessage')
-    return HttpResponse(userMessage)
+    chatResponse=str(bot.get_response(userMessage))
+    return HttpResponse(chatResponse)
